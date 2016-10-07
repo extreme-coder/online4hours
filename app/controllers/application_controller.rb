babbe1e9b5259e6550da029d36270cfed080dae7
Aryansingh singh <Aryansingh singh>
2016-10-06 20:48:51 -0700

    Updated the text and made an about page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :show_flash
 
   private
 
-  def show_flash
-    flash.now[:notice] = "Found the about page!" if request.path == '/pages/about'
-  end
+
 end
